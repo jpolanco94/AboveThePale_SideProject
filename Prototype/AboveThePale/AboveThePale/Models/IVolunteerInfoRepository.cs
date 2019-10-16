07244dfb9c33cf33e4000792e6915b9e5ac5683a
Jorge Polanco <Jorge.L.Polanco@outlook.com>
2019-10-15 17:19:35 -0700

    Added a Prototype. The prototype only has my models and basic controllers for identity defined.

---
+++ Prototype/AboveThePale/AboveThePale/Models/IVolunteerInfoRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AboveThePale.Models
+{
+    public interface IVolunteerInfoRepository
+    {
+        IQueryable<VolunteerInfo> VolunteerInfos { get; }
+    }
+}
